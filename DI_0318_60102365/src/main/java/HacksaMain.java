41a4546823eeb5b40847831094afe500553b9f57
geonho0201 <geonho0201@naver.com>
2015-03-30 13:36:53 +0900

    Dependency Injection에 관한 수업 3월 30일

---
+++ DI_0318_60102365/src/main/java/HacksaMain.java
@@ -0,0 +1,28 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class HacksaMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//		Professor professor = new Professor("choi");
+//		Lecture lecture1 = new Lecture("소프트웨어 공학");
+		
+		ApplicationContext factory = new ClassPathXmlApplicationContext(
+				new String[]{"applicationContext.xml"});
+		
+		//
+		Professor professor = (Professor)factory.getBean("professor");
+		Lecture lecture2 = (Lecture)factory.getBean("lecture2");
+		Student student = (Student)factory.getBean("student");
+		
+		professor.addLecture(lecture2);
+		lecture2.addStudent(student);
+		professor.showLecture();
+
+		
+	}
+
+}
